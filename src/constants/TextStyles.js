a05bb53c9318a9730dda16769aad794c08e5d55e
zhco00 <85988984+zhco00@users.noreply.github.com>
2024-02-01 01:59:31 +0900

    주제입력, 모달추가
    주제입력 페이지 및 모달, goback추가

--- src/constants/TextStyles.js
+++ src/constants/TextStyles.js
@@ -1,27 +1,23 @@
 import { StyleSheet } from 'react-native';
 import Colors from './Colors';
 
-const baseStyle = {
-    fontSize: 14,
-    fontFamily: 'NotoSansEN',
-    color: Colors.black,
-};
-
 export const TextStyles = StyleSheet.create({
     normal: {
-        ...baseStyle,
+        fontSize: 14,
         fontWeight: 'normal',
+        fontFamily: 'NotoSansEN',
+        color: Colors.black,
     },
     semiBold: {
-        ...baseStyle,
-        fontWeight: 'bold' // semi-bold
+        fontSize: 14,
+        fontWeight: '500', // semi-bold
+        fontFamily: 'NotoSansEN',
+        color: Colors.black,
     },
     title: {
-        ...baseStyle,
         fontSize: 18,
-        fontWeight: 'bold',
-    },
-    placeholder :{
-        color : Colors.grey
+        fontWeight: '500',
+        fontFamily: 'NotoSansEN',
+        color: Colors.black,
     },
 });

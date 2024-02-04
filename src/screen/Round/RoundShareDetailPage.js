3c3cf8ad65ab41d410fb8d1aeb8d270603c2366e
MarieSJLEE <marie.sjlee@gmail.com>
2024-02-04 22:10:42 +0900

    Share에 디테일 페이지 연결

---
+++ src/screen/Round/RoundShareDetailPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const RoundSendDetailPage = () => {
+  return <Text style={styles.text}>회차별 디테일 페이지</Text>;
+};
+export default RoundSendDetailPage;
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+  },
+});

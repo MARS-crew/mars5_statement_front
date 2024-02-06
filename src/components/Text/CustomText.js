8ce84b5ae0c142d88019cc2f9ef9cc91384a0834
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-02-06 20:02:32 +0900

    Delete src directory

--- src/components/Text/CustomText.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native';
-
-const CustomText = ({style, children, fontSize, fontColor}) => {
-  return <Text style={[{fontSize, color: fontColor}, style]}>{children}</Text>;
-};
-
-export default CustomText;
--- src/components/image/RemoteImage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Image} from 'react-native';
-
-const RemoteImage = ({uri, style}) => {
-  return <Image source={{uri: uri}} style={style} />;
-};
-
-export default RemoteImage;

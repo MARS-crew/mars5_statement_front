1cb82c3713ac6da43e03fdfa14eb025c84af0369
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-02-06 19:49:12 +0900

    Delete src/Components directory

--- src/Components/Image/LocalImage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Image} from 'react-native';
-
-const LocalImage = ({source, style}) => {
-  return <Image source={source} style={style} />;
-};
-
-export default LocalImage;

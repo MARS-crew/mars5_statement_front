6dd303cfda2629da3fb4e25e8484ef114c283602
MarieSJLEE <marie.sjlee@gmail.com>
2024-02-12 19:51:32 +0900

    메인페이지 Send, Share 조회 완료

--- src/navigation/TopTabNavigation.js
+++ src/navigation/TopTabNavigation.js
@@ -11,9 +11,6 @@ import {useTextType} from '../../context/TextTypeContext';
 import FloatingButton from '../components/button/FloatingButton';
 import {useNavigation} from '@react-navigation/native';
 
-import {getSuggest} from '../api/GetData';
-import {useLogin} from '../context/AuthContext';
-
 const Tab = createMaterialTopTabNavigator();
 
 const TopTabNavigator = () => {
@@ -52,39 +49,54 @@ const TopTabNavigator = () => {
   const handlePress = () => {
     navigation.navigate('NewTopicPage');
   };
+  // const {isLogin} = useLogin();
+  // const groupId = 2; // 임의 그룹 아이디 설정
+  // const [shareData, setShareData] = useState([]);
+  // const [sendData, setSendData] = useState([]);
+
+  // useEffect(() => {
+  //   if (isLogin) {
+  //     const loadData = async () => {
+  //       try {
+  //         const responseData = await getSuggest(groupId);
+  //         const shareFiltered = responseData.data.groupSuggests.filter(
+  //           item => item.type === 'share',
+  //         );
+  //         const sendFiltered = responseData.data.groupSuggests.filter(
+  //           item => item.type === 'send',
+  //         );
+  //         setShareData(shareFiltered);
+  //         setSendData(sendFiltered);
+  //       } catch (error) {
+  //         console.error('데이터 조회 실패:', error);
+  //       }
+  //     };
+  //     loadData();
+  //   }
+  // }, [isLogin, groupId]);
+
+  // if (!isLogin) {
+  //   return null;
+  // }
   return (
     <View style={{flex: 1}}>
       <Tab.Navigator
         screenOptions={{
           tabBarStyle: {
+            // backgroundColor: Colors.white,
             backgroundColor: Colors.white,
             shadowColor: 'transparent',
-            // height: scale(44),
-            height: 60,
+            height: scale(44),
           },
-          // tabBarLabelStyle: TextStyles.semiBold,
-          tabBarLabelStyle: {
-            fontSize: 14,
-            fontWeight: '500',
-            fontFamily: 'NotoSansEN',
-            color: Colors.black,
-          },
+          tabBarLabelStyle: TextStyles.semiBold,
           tabBarAndroidRipple: {borderless: false},
           tabBarIndicatorStyle: {
             borderBottomWidth: 2,
             borderBottomColor: Colors.green,
           },
         }}>
-        <Tab.Screen
-          name="Share"
-          component={Share}
-          // initialParams={{data: shareData}} // Share 탭에 shareData 전달
-        />
-        <Tab.Screen
-          name="Send"
-          component={Send}
-          // initialParams={{data: sendData}} // Send 탭에 sendData 전달
-        />
+        <Tab.Screen name="Share" component={Share} />
+        <Tab.Screen name="Send" component={Send} />
       </Tab.Navigator>
       <FloatingButton onPress={handlePress} />
     </View>

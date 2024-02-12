5165b51a2599a2aad3697791d95b841263d6cd46
MarieSJLEE <marie.sjlee@gmail.com>
2024-02-12 19:51:32 +0900

    메인페이지 Send, Share 조회 완료

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,18 +1,42 @@
-import {func} from 'prop-types';
-import React, {createContext, useContext, useState} from 'react';
+import React, {createContext, useContext, useState, useEffect} from 'react';
+import {getSuggest} from '../api/GetData';
 
 const Context = createContext({});
 
-export function AuthProvider({children}) {
+export const AuthProvider = ({children}) => {
   const [isLogin, setIsLogin] = useState(false);
+  const [data, setData] = useState({
+    shareData: [],
+    sendData: [],
+  });
+
+  useEffect(() => {
+    const loadData = async () => {
+      if (isLogin) {
+        try {
+          const groupId = 2; // 예시 그룹 ID
+          const responseData = await getSuggest(groupId);
+          const shareFiltered = responseData.data.groupSuggests.filter(
+            item => item.type === 'share',
+          );
+          const sendFiltered = responseData.data.groupSuggests.filter(
+            item => item.type === 'send',
+          );
+          setData({shareData: shareFiltered, sendData: sendFiltered});
+        } catch (error) {
+          console.error('데이터 조회 실패:', error);
+        }
+      }
+    };
+
+    loadData();
+  }, [isLogin]);
 
   return (
-    <Context.Provider value={{isLogin, setIsLogin}}>
+    <Context.Provider value={{isLogin, setIsLogin, data}}>
       {children}
     </Context.Provider>
   );
-}
+};
 
-export function useLogin() {
-  return useContext(Context);
-}
+export const useLogin = () => useContext(Context);
--- src/navigation/TopTabNavigation.js
+++ src/navigation/TopTabNavigation.js
@@ -1,15 +1,18 @@
 // TopTabNavigator.js
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import Send from '../screen/home/Send';
 import Share from '../screen/home/Share';
 import Colors from '../constants/Colors';
-import {moderateScale, scale} from '../constants/Scale';
+import {scale} from '../constants/Scale';
 import {TextStyles} from '../constants/TextStyles';
 import {Dimensions, View} from 'react-native';
 import {useTextType} from '../../context/TextTypeContext';
 import FloatingButton from '../components/button/FloatingButton';
 import { useNavigation } from '@react-navigation/native';
+
+import {getSuggest} from '../api/GetData';
+import {useLogin} from '../context/AuthContext';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -19,25 +22,68 @@ const TopTabNavigator = () => {
   const handlePress = () => {
     navigation.navigate('NewTopicPage');
   };
+  // const {isLogin} = useLogin();
+  // const groupId = 2; // 임의 그룹 아이디 설정
+  // const [shareData, setShareData] = useState([]);
+  // const [sendData, setSendData] = useState([]);
+
+  // useEffect(() => {
+  //   if (isLogin) {
+  //     const loadData = async () => {
+  //       try {
+  //         const responseData = await getSuggest(groupId);
+  //         const shareFiltered = responseData.data.groupSuggests.filter(
+  //           item => item.type === 'share',
+  //         );
+  //         const sendFiltered = responseData.data.groupSuggests.filter(
+  //           item => item.type === 'send',
+  //         );
+  //         setShareData(shareFiltered);
+  //         setSendData(sendFiltered);
+  //       } catch (error) {
+  //         console.error('데이터 조회 실패:', error);
+  //       }
+  //     };
+  //     loadData();
+  //   }
+  // }, [isLogin, groupId]);
+
+  // if (!isLogin) {
+  //   return null;
+  // }
   return (
     <View style={{flex : 1}}>
     <Tab.Navigator
       screenOptions={{
         tabBarStyle: {
-          // backgroundColor: Colors.white,
           backgroundColor: Colors.white,
           shadowColor: 'transparent',
-          height: scale(44),
+          // height: scale(44),
+          height: 60,
         },
-        tabBarLabelStyle: TextStyles.semiBold,
+        // tabBarLabelStyle: TextStyles.semiBold,
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: '500',
+          fontFamily: 'NotoSansEN',
+          color: Colors.black,
+        },
         tabBarAndroidRipple: {borderless: false},
         tabBarIndicatorStyle: {
           borderBottomWidth: 2,
           borderBottomColor: Colors.green,
         },
       }}>
-      <Tab.Screen name="Share" component={Share} />
-      <Tab.Screen name="Send" component={Send} />
+      <Tab.Screen
+        name="Share"
+        component={Share}
+        // initialParams={{data: shareData}} // Share 탭에 shareData 전달
+      />
+      <Tab.Screen
+        name="Send"
+        component={Send}
+        // initialParams={{data: sendData}} // Send 탭에 sendData 전달
+      />
     </Tab.Navigator>
     <FloatingButton onPress={handlePress} />
     </View>

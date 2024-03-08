f4b193f35e1bfd180371c40dbdc64e668cf3dc2f
nnnnnnniam1 <101879140+nnnnnnniam1@users.noreply.github.com>
2024-03-08 21:10:06 +0900

    그룹 멤버 모달

---
+++ src/components/modal/MemberModal.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Modal,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from 'react-native';
+import Colors from '../../constants/Colors';
+import {moderateScale} from '../../constants/Scale';
+
+const MemberModal = ({isVisible, setVisible, onClose, MemberData}) => {
+  return (
+    <Modal
+      // animationType="fade"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={onClose}>
+      <TouchableOpacity
+        onPress={() => setVisible(false)}
+        style={styles.modalBackdropPress}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            {MemberData.map((member, index) => (
+              <Image
+                key={index}
+                style={styles.logo}
+                source={{uri: member.img}}
+              />
+            ))}
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Modal>
+  );
+};
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    top: 70,
+    left: 80,
+    alignItems: 'flex-start',
+  },
+  modalContent: {
+    backgroundColor: Colors.blue,
+    padding: 10,
+    paddingLeft: 20,
+    borderRadius: 10,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    maxWidth: 320,
+  },
+  modalBackdropPress: {
+    flex: 1,
+  },
+  logo: {
+    width: moderateScale(44),
+    height: moderateScale(44),
+    marginRight: 10,
+    marginBottom: 10,
+  },
+});
+
+export default MemberModal;
--- src/components/text/LogoTitle.js
+++ src/components/text/LogoTitle.js
@@ -1,24 +1,50 @@
 // LogoTitle.js
-import React from 'react';
-import {View, Image, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import Colors from '../../constants/Colors';
 import {TextStyles} from '../../constants/TextStyles';
 import {moderateScale} from '../../constants/Scale';
+import MemberModal from '../modal/MemberModal';
 
-const LogoTitle = ({team}) => (
-  <View style={styles.container}>
-    <Image
-      style={styles.logo}
-      source={team.img ? {uri: team.img} : require('../../assest/images/image2.png')}
-    />
-    <Text style={[TextStyles.title]}>{team.name}</Text>
-  </View>
-);
+const LogoTitle = ({team}) => {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          setModalVisible(!isModalVisible);
+        }}>
+        <View style={styles.contents}>
+          <Image
+            style={styles.logo}
+            source={
+              team[0].img
+                ? {uri: team[0].img}
+                : require('../../assest/images/image2.png')
+            }
+          />
+          <Text style={[TextStyles.title]}>{team[0].name}</Text>
+        </View>
+      </TouchableOpacity>
+      <MemberModal
+        isVisible={isModalVisible}
+        setVisible={setModalVisible}
+        onClose={() => setModalVisible(false)}
+        MemberData={team[1]}
+      />
+    </View>
+  );
+};
 
 export default LogoTitle;
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  contents: {
     flexDirection: 'row',
     alignItems: 'center',
   },
